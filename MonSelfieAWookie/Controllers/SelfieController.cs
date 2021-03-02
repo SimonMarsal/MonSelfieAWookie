6ad4627da0a05a67e13edfbf2484a7da80d1833d
SimonMarsal <79854109+SimonMarsal@users.noreply.github.com>
2021-03-02 15:00:50 +0100

    - Passage en paramètre d'une liste de modèle du ocntroleur vers la vue - Assignation à la vue du type de modèle : @model List<Selfie>

--- MonSelfieAWookie/Controllers/SelfieController.cs
+++ MonSelfieAWookie/Controllers/SelfieController.cs
@@ -22,7 +22,14 @@ namespace MonSelfieAWookie.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            //Liste de models selfie
+            List<Selfie> selfies = new List<Selfie>() {
+
+            new Selfie(){ Id=1, Url="https://c1.staticflickr.com/1/39/85740389_00e3dfb5bf_b.jpg", Titre = "Ioda"},
+            new Selfie(){ Id=2, Url="https://c1.staticflickr.com/1/779/31649499113_374ef73b32_b.jpg", Titre="Chewbacca"}
+            };
+
+            return View(selfies);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
