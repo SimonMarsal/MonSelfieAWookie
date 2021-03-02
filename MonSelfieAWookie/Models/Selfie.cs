d911326225db30734f1c9a6dde00edfdc741760d
SimonMarsal <79854109+SimonMarsal@users.noreply.github.com>
2021-03-02 14:30:02 +0100

    razor : foreach sur liste d'objets

---
+++ MonSelfieAWookie/Models/Selfie.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MonSelfieAWookie.Models
+{
+    public class Selfie
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Titre { get; set; }
+    }
+}
