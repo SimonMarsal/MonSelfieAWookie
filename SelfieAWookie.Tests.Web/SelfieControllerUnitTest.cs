1d6357842056c816af2137e7e775f23c62173a9c
SimonMarsal <79854109+SimonMarsal@users.noreply.github.com>
2021-03-03 15:07:44 +0100

    repo selfie / json ok

--- SelfieAWookie.Tests.Web/SelfieControllerUnitTest.cs
+++ SelfieAWookie.Tests.Web/SelfieControllerUnitTest.cs
@@ -7,6 +7,7 @@ using SelfieAWookie.Core.Domain;
 
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -31,7 +32,8 @@ namespace SelfieAWookie.Tests.Web
         {
             public IList<Selfie> GetAll()
             {
-                string jsonResult = File.ReadAllText("Resources/Json/TestSelfies.json");
+                string filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"TestSelfies.json");
+                string jsonResult = File.ReadAllText(filePath);
                 return JsonSerializer.Deserialize<List<Selfie>>(jsonResult);
             }
         }
