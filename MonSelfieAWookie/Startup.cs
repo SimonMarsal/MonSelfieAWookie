8dce3fc4b94c6ddaa2dfc3d48b2d53a67e7e8987
SimonMarsal <79854109+SimonMarsal@users.noreply.github.com>
2021-03-03 15:48:26 +0100

    suite

--- MonSelfieAWookie/Startup.cs
+++ MonSelfieAWookie/Startup.cs
@@ -31,7 +31,7 @@ namespace MonSelfieAWookie
             services.AddControllersWithViews();
 
             services.AddScoped<IWookieRepository, MemoryWookieRepository>();
-            services.AddScoped<ISelfieRepository, JsonSelfieRepository>();
+            services.AddScoped<ISelfieRepository, MemorySelfieRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
