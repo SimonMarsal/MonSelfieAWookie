ff4759ecedb1fd7767183eba24d6bb5bd9a4ac14
SimonMarsal <79854109+SimonMarsal@users.noreply.github.com>
2021-03-03 12:08:25 +0100

    Création IRepository
    utilisation dans le test

---
+++ SelfieAWookie.Core.Domain/IWookieRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelfieAWookie.Core.Domain
+{
+    public interface IWookieRepository
+    {
+        IList<Wookie> GetAll();
+    }
+}
--- SelfieAWookie.Tests.Web/WookieControllerUnitTest.cs
+++ SelfieAWookie.Tests.Web/WookieControllerUnitTest.cs
@@ -1,22 +1,81 @@
-using System;
-
-using Xunit;
+using Microsoft.AspNetCore.Mvc;
 
 using MonSelfieAWookie.Controllers;
-using Microsoft.AspNetCore.Mvc;
-using MonSelfieAWookie.Models.Dtos;
 using MonSelfieAWookie.Models;
+
+using SelfieAWookie.Core.Domain;
+
+using System.Collections.Generic;
+
+using Xunit;
 
 namespace SelfieAWookie.Tests.Web
 {
     public class WookieControllerUnitTest
     {
+        private class FakeWookieRepository : IWookieRepository
+        {
+            public IList<Wookie> GetAll()
+            => new List<Wookie>()
+            {
+                new Wookie(){ Id=1, Prenom="Ioda", NbPoils=655, Selfies = new List<Selfie>()
+                    {
+                        new Selfie(){ Id=1, Titre="ioda", Url="https://c1.staticflickr.com/1/39/85740389_00e3dfb5bf_b.jpg"}
+                    },
+                    MainWeapon = new Weapon(){ Id=1, Label="Sabre vert"},
+                    SecondaryWeapons = new List<Weapon>()
+                    {
+                        new Weapon(){ Id=2, Label="CrossBow"},
+                        new Weapon(){ Id=3, Label="RainBow"}
+                    }
+                },
+
+                new Wookie(){ Id=2, Prenom="Chew", NbPoils=8985654, Selfies = new List<Selfie>()
+                    {
+                        new Selfie(){ Id=1, Titre="Chew", Url="https://c1.staticflickr.com/1/779/31649499113_374ef73b32_b.jpg"},
+                        new Selfie(){ Id=2, Titre="Chew", Url="https://c1.staticflickr.com/1/779/31649499113_374ef73b32_b.jpg"},
+                        new Selfie(){ Id=3, Titre="Chew", Url="https://c1.staticflickr.com/1/779/31649499113_374ef73b32_b.jpg"},
+                        new Selfie(){ Id=4, Titre="Chew", Url="https://c1.staticflickr.com/1/779/31649499113_374ef73b32_b.jpg"},
+                    },
+                    MainWeapon = new Weapon(){ Id=1, Label="Bow"},
+                    SecondaryWeapons = new List<Weapon>()
+                    {
+                        new Weapon(){ Id=2, Label="knife"},
+                        new Weapon(){ Id=3, Label="fist"}
+                    }
+                },
+
+                new Wookie(){ Id=3, Prenom="Obiwan", NbPoils=45, Selfies = new List<Selfie>()
+                    {
+                        new Selfie(){ Id=1, Titre="Obiwan", Url="https://c1.staticflickr.com/1/779/31649499113_374ef73b32_b.jpg"},
+                        new Selfie(){ Id=2, Titre="Obiwan", Url="https://c1.staticflickr.com/1/779/31649499113_374ef73b32_b.jpg"},
+                        new Selfie(){ Id=3, Titre="Obiwan", Url="https://c1.staticflickr.com/1/779/31649499113_374ef73b32_b.jpg"},
+                        new Selfie(){ Id=4, Titre="Obiwan", Url="https://c1.staticflickr.com/1/779/31649499113_374ef73b32_b.jpg"},
+                        new Selfie(){ Id=5, Titre="Obiwan", Url="https://c1.staticflickr.com/1/779/31649499113_374ef73b32_b.jpg"}
+                    },
+                    MainWeapon = new Weapon(){ Id=1, Label="Sabre bleu"},
+                    SecondaryWeapons = new List<Weapon>()
+                    {
+                        new Weapon(){ Id=2, Label="knife"},
+                        new Weapon(){ Id=3, Label="fist"},
+                        new Weapon(){ Id=4, Label="fist"},
+                        new Weapon(){ Id=5, Label="fist"},
+                        new Weapon(){ Id=6, Label="fist"},
+                        new Weapon(){ Id=7, Label="fist"},
+                    }
+
+                },
+            };
+        }
+
         #region Public Methods
         [Fact]
         public void ShouldReturnListOfWookies()
         {
+            IWookieRepository fakeRepo = new FakeWookieRepository();
+
             //1-Arrange
-            WookieController ctrl = new WookieController();
+            WookieController ctrl = new WookieController(fakeRepo);
 
             //2-Act
             //Index renvoie un ViewResult
